0d29473d3b4a81faa354773b1b770cecd85a0fc3
kznleaf <kunpengdu43@gmail.com>
2026-03-31 20:42:08 +0800

    toggle cursorline

--- lua/kznleaf/plugins/colorscheme.lua
+++ lua/kznleaf/plugins/colorscheme.lua
@@ -2,7 +2,11 @@ return {
 	"olimorris/onedarkpro.nvim",
 	priority = 1000, -- 确保主题在其他插件之前加载
 	config = function()
-		require("onedarkpro").setup({})
+		require("onedarkpro").setup({
+			options = {
+				cursorline = true,
+			},
+		})
 		vim.cmd("colorscheme onedark") -- 移动到这里
 	end,
 }
