e3b3a9873dbb71d82021b5973e830c92f56630b0
kznLeaf <kunpengdu43@gmail.com>
2026-04-04 16:09:11 +0800

    add textobjects keymap

---
+++ lua/kznleaf/plugins/textobjects.lua
@@ -0,0 +1,126 @@
+return {
+	"nvim-treesitter/nvim-treesitter-textobjects",
+	branch = "main",
+	init = function()
+		-- Disable entire built-in ftplugin mappings to avoid conflicts.
+		-- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
+		vim.g.no_plugin_maps = true
+
+		-- Or, disable per filetype (add as you like)
+		-- vim.g.no_python_maps = true
+		-- vim.g.no_ruby_maps = true
+		-- vim.g.no_rust_maps = true
+		-- vim.g.no_go_maps = true
+	end,
+	config = function()
+		-- put your config here
+		local textobjects = require("nvim-treesitter-textobjects")
+		textobjects.setup({
+			select = {
+				-- Automatically jump forward to textobj, similar to targets.vim
+				lookahead = true,
+				-- You can choose the select mode (default is charwise 'v')
+				--
+				-- Can also be a function which gets passed a table with the keys
+				-- * query_string: eg '@function.inner'
+				-- * method: eg 'v' or 'o'
+				-- and should return the mode ('v', 'V', or '<c-v>') or a table
+				-- mapping query_strings to modes.
+				selection_modes = {
+					["@parameter.outer"] = "v", -- charwise
+					["@function.outer"] = "V", -- linewise
+					-- ['@class.outer'] = '<c-v>', -- blockwise
+				},
+				-- If you set this to `true` (default is `false`) then any textobject is
+				-- extended to include preceding or succeeding whitespace. Succeeding
+				-- whitespace has priority in order to act similarly to eg the built-in
+				-- `ap`.
+				--
+				-- Can also be a function which gets passed a table with the keys
+				-- * query_string: eg '@function.inner'
+				-- * selection_mode: eg 'v'
+				-- and should return true of false
+				include_surrounding_whitespace = false,
+			},
+			move = {
+				set_jumps = true,
+			},
+		})
+		-- keymaps
+		-- You can use the capture groups defined in `textobjects.scm`
+		vim.keymap.set({ "x", "o" }, "af", function()
+			require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
+		end)
+		vim.keymap.set({ "x", "o" }, "if", function()
+			require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
+		end)
+		vim.keymap.set({ "x", "o" }, "ac", function()
+			require("nvim-treesitter-textobjects.select").select_textobject("@class.outer", "textobjects")
+		end)
+		vim.keymap.set({ "x", "o" }, "ic", function()
+			require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", "textobjects")
+		end)
+		-- You can also use captures from other query groups like `locals.scm`
+		vim.keymap.set({ "x", "o" }, "as", function()
+			require("nvim-treesitter-textobjects.select").select_textobject("@local.scope", "locals")
+		end)
+		-- 选中参数内部 (类似 Zed 的 ia)
+		vim.keymap.set({ "x", "o" }, "ia", function()
+			require("nvim-treesitter-textobjects.select").select_textobject("@parameter.inner", "textobjects")
+		end)
+		-- 选中参数及其周围（包含逗号和空格，类似 Zed 的 aa）
+		vim.keymap.set({ "x", "o" }, "aa", function()
+			require("nvim-treesitter-textobjects.select").select_textobject("@parameter.outer", "textobjects")
+		end)
+
+		---------------- 移动相关的快捷键 -------------------
+		-- 跳转到上/下一个函数开头的位置
+		vim.keymap.set({ "n", "x", "o" }, "]m", function()
+			require("nvim-treesitter-textobjects.move").goto_next_start("@function.outer", "textobjects")
+		end)
+		vim.keymap.set({ "n", "x", "o" }, "[m", function()
+			require("nvim-treesitter-textobjects.move").goto_previous_start("@function.outer", "textobjects")
+		end)
+		-- 跳转到上/下一个函数结尾的位置
+		vim.keymap.set({ "n", "x", "o" }, "]M", function()
+			require("nvim-treesitter-textobjects.move").goto_next_end("@function.outer", "textobjects")
+		end)
+		vim.keymap.set({ "n", "x", "o" }, "[M", function()
+			require("nvim-treesitter-textobjects.move").goto_previous_end("@function.outer", "textobjects")
+		end)
+
+		-- 跳转到下一个class/struct定义的开头
+		vim.keymap.set({ "n", "x", "o" }, "]]", function()
+			require("nvim-treesitter-textobjects.move").goto_next_start("@class.outer", "textobjects")
+		end)
+		-- 跳转到上一个class/struct定义的开头
+		vim.keymap.set({ "n", "x", "o" }, "[[", function()
+			require("nvim-treesitter-textobjects.move").goto_previous_start("@class.outer", "textobjects")
+		end)
+
+		-- 跳转到下一个类/结构体定义的结尾
+		vim.keymap.set({ "n", "x", "o" }, "][", function()
+			require("nvim-treesitter-textobjects.move").goto_next_end("@class.outer", "textobjects")
+		end)
+		-- 跳转到上一个类/结构体定义的结尾
+		vim.keymap.set({ "n", "x", "o" }, "[]", function()
+			require("nvim-treesitter-textobjects.move").goto_previous_end("@class.outer", "textobjects")
+		end)
+		-- 跳转到下一个循环的开头
+		vim.keymap.set({ "n", "x", "o" }, "]o", function()
+			require("nvim-treesitter-textobjects.move").goto_next_start({ "@loop.inner", "@loop.outer" }, "textobjects")
+		end)
+
+		------------------- 重复操作的快捷键 -------------------
+		local ts_repeat_move = require("nvim-treesitter-textobjects.repeatable_move")
+
+		-- Repeat movement with ; and ,
+		-- ensure ; goes forward and , goes backward regardless of the last direction
+		vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
+		vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
+
+		-- vim way: ; goes to the direction you were moving. f F t T
+		-- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+		-- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+	end,
+}
--- lua/kznleaf/plugins/treesitter.lua
+++ lua/kznleaf/plugins/treesitter.lua
@@ -1,29 +1,42 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-  },
-  config = function()
-    local ts = require("nvim-treesitter")
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+	},
+	config = function()
+		local ts = require("nvim-treesitter")
 
-    ts.install({
-      "go", "gomod", "gowork", "gosum", 
-      "rust", "python", "proto",       
-      "lua", "vim", "vimdoc", "query", "c",
-      "markdown", "markdown_inline",
-      "json", "yaml", "bash", "dockerfile", "gitignore",
-    })
+		ts.install({
+			"go",
+			"gomod",
+			"gowork",
+			"gosum",
+			"rust",
+			"python",
+			"proto",
+			"lua",
+			"vim",
+			"vimdoc",
+			"query",
+			"c",
+			"markdown",
+			"markdown_inline",
+			"json",
+			"yaml",
+			"bash",
+			"dockerfile",
+			"gitignore",
+		})
 
-    vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
-      callback = function()
-        local bufnr = vim.api.nvim_get_current_buf()
-        pcall(vim.treesitter.start, bufnr)
-      end,
-    })
+		vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+			callback = function()
+				local bufnr = vim.api.nvim_get_current_buf()
+				pcall(vim.treesitter.start, bufnr)
+			end,
+		})
 
-    require('nvim-ts-autotag').setup()
-    
-  end,
+		require("nvim-ts-autotag").setup()
+	end,
 }
